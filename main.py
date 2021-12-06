d2ded7be19e1f0446a1e7edc88e169a9d90d1d03
Anudeep Yegireddi <anudeepyegireddi@Anudeeps-MacBook-Pro.local>
2021-12-06 13:37:44 +0530

    Simple hello world program compiled from Neo4J's documentation.

---
+++ main.py
@@ -0,0 +1,27 @@
+from neo4j import GraphDatabase
+
+class HelloWorldExample:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def print_greeting(self, message):
+        with self.driver.session() as session:
+            greeting = session.write_transaction(self._create_and_return_greeting, message)
+            print(greeting)
+
+    @staticmethod
+    def _create_and_return_greeting(tx, message):
+        result = tx.run("CREATE (a:Greeting) "
+                        "SET a.message = $message "
+                        "RETURN a.message + ', from node ' + id(a)", message=message)
+        return result.single()[0]
+
+
+if __name__ == "__main__":
+    greeter = HelloWorldExample("neo4j+s://8219a15b.databases.neo4j.io:7687", "neo4j", "SHHcaUZYNPF-qWoyOrahHksjOelYBFASgdqjrRd1Ju8")
+    greeter.print_greeting("hello, world")
+    greeter.close()
